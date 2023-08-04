2fb526c467d830c0900116a8374e08de2902ca25
ruyla <ruyla@proton.me>
2023-08-04 11:38:43 -0500

    Type updates

--- src/types/global.d.tsx
+++ src/types/global.d.tsx
@@ -1,13 +1,29 @@
+import { ChangeEventHandler } from "react"
+
 export { }
 
+/**
+ * type1 is for button elements.
+ * type2 is for input elements.
+ */
 declare global {
     interface IComponent {
         children: React.ReactNode
         display: string
+        element: 'input' | 'textarea' | string | undefined
+        error: string
+        htmlFor: string
+        id: string
         image: string
+        label: string
         link: string
+        name: string
+        onChange: ChangeEventHandler<HTMLInputElement>
+        placeholder: string
         target: 'internal' | 'external' | undefined
         title: string
-        type: 'button' | 'submit' | 'reset' | undefined
+        type1: 'button' | 'submit' | 'reset' | undefined
+        type2: 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week' | string | undefined
+        value: string | number | undefined
     }
 }

1fdebcd37ecc7eb70688c3ac48fb65d07c3ab1e1
ruyla <ruyla@proton.me>
2023-08-14 00:26:48 -0500

    Added some grid stuff

--- src/app/en/leasing/properties/[city]/[property]/page.tsx
+++ src/app/en/leasing/properties/[city]/[property]/page.tsx
@@ -50,31 +50,38 @@ const Page = ({ params, searchParams }: Props) => {
                     return (
                         <div className={styles.wrapper}
                             key={index}>
-                            <div className={styles.heading}>
-                                <h4>{value.displayName}</h4>
-                                <h5>{value.address.street}, {value.address.city}, {value.address.state} {value.address.zipCode}</h5>
+                            {/* This is our heading. */}
+                            <div className={styles.header}>
+                                <h5>{value.displayName}</h5>
+                                <h6>{value.address.street}, {value.address.city}, {value.address.state} {value.address.zipCode}</h6>
                             </div>
+                            {/* This is our slideshow. */}
                             <div className={styles.slideshow}>
                                 <Slideshow
                                     values={value.images} />
                             </div>
-                            <div className={styles.description}>
-                                {value.description}
+                            {/* This is our overview. */}
+                            <div className={styles.overview}>
+                                <h6>Property Overview</h6>
+                                <p>{value.description}</p>
                             </div>
-                            <div className={styles.people}>
-
+                            {/* These are our property managers and engineers. */}
+                            <div className={styles.staff}>
+                                <p>Property Staff</p>
                             </div>
+                            {/* These are our available spaces. */}
                             <div className={styles.availableSpaces}>
-
+                                <p>Available Spaces</p>
                             </div>
+                            {/* These are our reviews. */}
                             <div className={styles.reviews}>
-
+                                <p>Reviews</p>
                             </div>
                             <div className={styles.amenities}>
-
+                                <p>Amenities</p>
                             </div>
                             <div className={styles.hours}>
-
+                                <p>Hours</p>
                             </div>
                             <div className={styles.resources}>
                                 <h5>Resources</h5>
